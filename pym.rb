17cb28717273315a74ae86b33416411d0c792a08
c-bata <contact@c-bata.link>
2015-10-28 00:39:14 +0900

    Update v0.0.4

--- pym.rb
+++ pym.rb
@@ -1,9 +1,10 @@
 class Pym < Formula
   desc "Simple python interpreter manager"
   homepage "https://github.com/c-bata/pym"
-  url "https://github.com/c-bata/pym/releases/download/v0.0.3/pym"
-  sha256 "5e687a3185d3d1d3d60d5070be4305f551d7acb74efaefb2c86288d213e417e1"
-  version "0.0.3"
+  url "https://github.com/c-bata/pym/releases/download/v0.0.4/pym"
+  # openssl dgst -sha256 pym
+  sha256 "93c513e70c271361e332c1383f3b95ee618e4679c9eb03449162bb5ae910d8ec"
+  version "0.0.4"
 
   def install
     bin.install 'pym'
